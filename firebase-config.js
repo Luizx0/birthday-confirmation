8d45d95619484754b7ccfc66adc720e2b785e8e5
Luizx0 <Loue0982@gmail.com>
2025-02-26 17:52:34 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -6,9 +6,9 @@ import { getDatabase, ref, set, get, child, onValue } from "https://www.gstatic.
 const firebaseConfig = {
     apiKey: "AIzaSyC4AjavlEZA4mc3A8q1qlG3FwyXF6CqqOI",
     authDomain: "birthday-confirmation.firebaseapp.com",
-    databaseURL: "https://birthday-confirmation.firebaseio.com",
+    databaseURL: "https://birthday-confirmation-default-rtdb.firebaseio.com",
     projectId: "birthday-confirmation",
-    storageBucket: "birthday-confirmation.appspot.com",
+    storageBucket: "birthday-confirmation.firebasestorage.app",
     messagingSenderId: "654032261381",
     appId: "1:654032261381:web:a5d5a8dd08f1027c06276c",
     measurementId: "G-1RBV35Z66Q"
